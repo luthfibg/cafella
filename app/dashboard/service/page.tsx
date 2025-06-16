0d779ba9e879c6687329432f9c01d12b56bcddf3
luthfibg <luthfinewzoneonline@gmail.com>
2025-06-16 12:30:46 +0700

    #2a fixing eslint warning err

--- app/dashboard/service/page.tsx
+++ app/dashboard/service/page.tsx
@@ -1,9 +1,7 @@
 import { Card } from '@/app/ui/card'
 import React from 'react'
 
-type Props = {}
-
-const page = (props: Props) => {
+const page = () => {
   return (
     <div className="flex gap-4 h-full w-full">
       <Card className='flex items-center justify-center h-full'>SERVICE Panel</Card>
